77cf7ea1c92312e34f223ee7279fcf790ab51a67
Kurokitu <foxiti.a@gmail.com>
2020-07-16 21:59:49 +0800

    Initial Commit

---
+++ VRCMakerX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  VRCMakerX
+//
+//  Created by kitu Kuro on 2020/7/16.
+//  Copyright © 2020 kitu Kuro. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ VRCMakerX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  VRCMakerX
+//
+//  Created by kitu Kuro on 2020/7/16.
+//  Copyright © 2020 kitu Kuro. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
